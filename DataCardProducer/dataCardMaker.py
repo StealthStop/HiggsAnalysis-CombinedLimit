597bba096baac6b68440faa621dd0dde3f7e87be
Alexander_Mercaldi <alexander.mercaldi@email.ucr.edu>
2020-08-22 02:28:32 -0500

    Added 2D functionality to produce data cards

--- DataCardProducer/dataCardMaker.py
+++ DataCardProducer/dataCardMaker.py
@@ -13,30 +13,31 @@ class dataCardMaker:
         self.fillBinValues()
         self.writeCards()
 
-    def fillBinValues(self):
-        self.nbins = 0
-        for sg in self.signal.keys():
-            binValues = []
-            tfile = ROOT.TFile.Open(self.path+self.signal[sg]["path"])
-            for hist in self.histos.keys():
-                h = tfile.Get(self.histos[hist]["name"])
-                if self.histos[hist]["end"] == "last":
-                    lastbin = h.GetNbinsX()
+    def calcBinValues(self, tfile):
+        binValues = []
+        for hist in self.histos.keys():
+            h = tfile.Get(self.histos[hist]["name"])
+            if isinstance(h, ROOT.TH2D) or isinstance(h, ROOT.TH2F):
+                if self.histos[hist]["end"][0] == "last":
+                    lastbinx = h.GetNbinsX()
                 else:
-                    lastbin = self.histos[hist]["end"]
-                histbins = lastbin - self.histos[hist]["start"] + 1
-                skip = histbins/self.histos[hist]["nbins"]
-                for bin in range(self.histos[hist]["start"], lastbin, skip):
-                    val = round(h.Integral(bin, bin + skip), 1)
-                    #print("integrating from {} to {}. Value = {}".format(bin, bin + skip, round(h.Integral(bin, bin + skip), 1)))
-                    if val < 1: val = 1
-                    binValues.append(val)
-            self.signal[sg]["binValues"] = binValues
-        for bg in self.background.keys():
-            binValues = []
-            tfile = ROOT.TFile.Open(self.path+self.background[bg]["path"])
-            for hist in self.histos.keys():
-                h = tfile.Get(self.histos[hist]["name"])
+                    lastbinx = self.histos[hist]["end"][0]
+                    
+                histbinsx = lastbinx - self.histos[hist]["start"][0]
+                skipx = histbinsx/self.histos[hist]["nbins"][0]
+                if self.histos[hist]["end"][1] == "last":
+                    lastbiny = h.GetNbinsY()
+                else:
+                    lastbiny = self.histos[hist]["end"][1]
+                    
+                histbinsy = lastbiny - self.histos[hist]["start"][1]
+                skipy = histbinsy/self.histos[hist]["nbins"][1]
+                for binx in range(self.histos[hist]["start"][0], lastbinx, skipx):
+                    for biny in range(self.histos[hist]["start"][1], lastbiny, skipy):
+                        val = round(h.Integral(binx, binx + skipx, biny, biny + skipy), 1)
+                        if val < 0.1: val = 0.1
+                        binValues.append(val)
+            else:
                 if self.histos[hist]["end"] == "last":
                     lastbin = h.GetNbinsX()
                 else:
@@ -46,12 +47,25 @@ class dataCardMaker:
                 skip = histbins/self.histos[hist]["nbins"]
                 for bin in range(self.histos[hist]["start"], lastbin, skip):
                     val = round(h.Integral(bin, bin + skip), 1)
-                    if val < 1: val = 1
+                    if val < 0.1: val = 0.1
                     binValues.append(val)
-            self.background[bg]["binValues"] = binValues
+            return binValues
+                    
+
+    def fillBinValues(self):
+        self.nbins = 0
+        for sg in self.signal.keys():
+            tfile = ROOT.TFile.Open(self.path+self.signal[sg]["path"])            
+            self.signal[sg]["binValues"] = self.calcBinValues(tfile)
+        for bg in self.background.keys():
+            tfile = ROOT.TFile.Open(self.path+self.background[bg]["path"])
+            self.background[bg]["binValues"] = self.calcBinValues(tfile)
         self.nbins = 0
         for h in self.histos.keys():
-            self.nbins += self.histos[h]["nbins"]
+            if isinstance(self.histos[h]["nbins"], list) and len(self.histos[h]["nbins"]) == 2:
+                self.nbins += self.histos[h]["nbins"][0]*self.histos[h]["nbins"][1]
+            else:
+                self.nbins += self.histos[h]["nbins"]
         self.observedPerBin = []        
         for n in range(self.nbins):
             obs = 0
